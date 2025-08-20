edcc4ddde28f42480b63f06b8d36bf7819d980ac
Adegbulu Ayomiposi <Adegbuluayomiposi@gmail.com>
2025-08-20 18:51:17 +0100

    mapped repetitive lines in placeholderbox to avoid repetition of code and redundancies

--- src/components/ModelCanvas.tsx
+++ src/components/ModelCanvas.tsx
@@ -1,4 +1,3 @@
-// src/components/ModelCanvas.tsx
 import React, { useRef, useEffect, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Html } from '@react-three/drei'
